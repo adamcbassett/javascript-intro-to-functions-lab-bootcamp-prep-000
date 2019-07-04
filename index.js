27daba0c9688668c19825a860fc0e0ffc6a22ccf
Adam Bassett <acbassettone@gmail.com>
2019-07-04 16:58:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
-  console.log (string.toUpperCase())
+  console.log (string.toUpperCase());
 }
 
 function logWhisper(string) {
-  console.log (string.toLowerCase())
+  console.log (string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
   if (string.LowerCase)
-  console.log (`I can\'t hear you!`)
+  console.log (`I can\'t hear you!`);
 
 else if (string.UpperCase)
-  console.log (`YES INDEED!`)
+  console.log (`YES INDEED!`);
 
 else if (string = `I love you, Grandma.`)
-  console.log (`I love you, too.`)
+  console.log (`I love you, too.`);
 }
