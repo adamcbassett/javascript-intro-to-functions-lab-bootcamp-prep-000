a70418a554d1a20d0a5b9969bdeaa867e75aac17
Adam Bassett <acbassettone@gmail.com>
2019-07-04 16:42:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase)()
+  if (string.toLowerCase)
   console.log (`I can\'t hear you!`)
 }
 
 function sayHiToGrandma(string) {
-  if (string.toUpperCase)()
+  if (string.toUpperCase)
   console.log (`YES INDEED!`)
 }
 
