d4e15e2a7cb207e26757adb076d0a4616a993ef6
Adam Bassett <acbassettone@gmail.com>
2019-07-04 16:27:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-let server
-before(function(done) {
-  this.enableTimeouts(false)  <----
-  server = require('../app')
-  server.initialize()
-    .then(() => {
-      console.info('listening on', process.env.PORT)
-      server.listen(process.env.PORT, done)
-    })
-    .catch(err => {
-      console.log(err)
-      done(err)
-    })
-})
-
 function shout(string) {
   return string.toUpperCase()
 }
