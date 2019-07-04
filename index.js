f50eabb8cd24e9110c91c3d7afced9ed255449df
Adam Bassett <acbassettone@gmail.com>
2019-07-04 17:18:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.LowerCase)
+  if (string.toLowerCase)
   return "`I can\'t hear you!`";
 
-else if (string.UpperCase)
+else if (string.toUpperCase)
   return "`YES INDEED!`";
 
 else if (string = `I love you, Grandma.`)
